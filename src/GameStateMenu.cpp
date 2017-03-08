4e40efce46a8f78a96c7b4b3230d8512b73a651c
Jacob Frank <jacobfrank4@gmail.com>
2017-03-07 19:32:17 -0800

    added menu assets and logic

--- src/GameStateMenu.cpp
+++ src/GameStateMenu.cpp
@@ -10,29 +10,36 @@
 #include "LTimer.h"
 #include "LTexture.h"
 #include "Window.h"
+#include <unistd.h>
 
 bool GameStateMenu::load() {
 
 	bool success = true;
 
-	//Open the font
-	this->frameFont = TTF_OpenFont( "assets/fonts/kenpixelsquare.ttf", 50);
-	if ( this->frameFont == NULL ) {
-		printf( "Failed to load font! SDL_ttf Error: %s\n", TTF_GetError() );
+	this->menuFont = TTF_OpenFont( "assets/fonts/Overdrive Sunset.otf", 110);
+	if ( this->menuFont == NULL ) {
+		printf( "Failed to load font - Overdrive Sunset.otf! SDL_ttf Error: %s\n", TTF_GetError() );
 		success = false;
 	}
 
-	this->menuFont = TTF_OpenFont( "assets/fonts/Overdrive Sunset.otf", 100);
-	if ( this->frameFont == NULL ) {
-		printf( "Failed to load font! SDL_ttf Error: %s\n", TTF_GetError() );
+	this->headingFont = TTF_OpenFont( "assets/fonts/SEGUISB.ttf", 30);
+	if ( this->headingFont == NULL ) {
+		printf( "Failed to load font - SEGUISB.TTF! SDL_ttf Error: %s\n", TTF_GetError() );
+		success = false;
+	}
+
+	this->textboxFont = TTF_OpenFont( "assets/fonts/SEGOEUISL.ttf", 30);
+	if ( this->textboxFont == NULL ) {
+		printf( "Failed to load font - SEGOEUISL.ttf! SDL_ttf Error: %s\n", TTF_GetError() );
 		success = false;
 	}
 
 	this->level = new Level();
 	if (!this->level->levelTexture.loadFromFile("assets/TitleScreen_Marz.png", this->game->renderer)) {
 		printf("Failed to load the level texture!\n");
+		success = false;
 	} else {
-		this->level->levelTexture.setDimensions(this->game->window->getWidth(), this->game->window->getHeight());	
+		this->level->levelTexture.setDimensions(this->game->window->getWidth(), this->game->window->getHeight());
 	}
 
 	this->camera = new Camera(this->game->window->getWidth(), this->game->window->getHeight());
@@ -42,50 +49,11 @@ bool GameStateMenu::load() {
 }
 
 void GameStateMenu::loop() {
-	//The frames per second timer
-	LTimer fpsTimer;
-
-	//The frames per second cap timer
-	LTimer capTimer;
-
-	//Keeps track of time between steps
-	LTimer stepTimer;
-
-	//Start counting frames per second
-	unsigned long countedFrames = 0;
-	float avgFPS = 0;
-	fpsTimer.start();
 
 	// State Loop
 	while (this->play) {
-		//Start cap timer
-		capTimer.start();
-
-		//Calculate and correct fps
-		avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
-
-		//Set FPS text to be rendered
-		this->frameTimeText.str( "" );
-		this->frameTimeText << std::fixed << std::setprecision(0) << "FPS: " << avgFPS;
-
-		// Process frame
 		this->handle();	// Handle user input
-		this->update(stepTimer.getTicks() / 1000.f); // Update state values
-		stepTimer.start(); //Restart step timer
-		this->sync();	// Sync game to server
 		this->render();	// Render game state to window
-
-		++countedFrames;
-
-		//If frame finished early
-		int frameTicks = capTimer.getTicks();
-		if ( frameTicks < SCREEN_TICK_PER_FRAME ) {
-			//Wait remaining time
-			SDL_Delay( SCREEN_TICK_PER_FRAME - frameTicks );
-		}
-
-		SDL_RenderPresent(this->game->renderer);
-
 	}
 }
 
@@ -95,92 +63,126 @@ void GameStateMenu::sync() {
 
 /**
 * Function: handle
-* 
-* Date: 
+*
+* Date:
 * JF: February 8, 2017: added handler for window resizing event
-* 
+*
 * Designer:
-* 
+*
 * Programmer:
 * Jacob Frank
-* 
+*
 * Interface: handle()
-* 
+*
 * Returns: void
-* 
+*
 * Notes:
 * Function handles events occuring within the window to control
 * functionality of the main menu
 */
 void GameStateMenu::handle() {
 	int x,y;
+	SDL_Keycode keyCode;
 
 	//Handle events on queue
-	while ( SDL_PollEvent( &this->event )) {
-		this->game->window->handleEvent(this->event);
-   		switch ( this->event.type ) {
-	      	case SDL_KEYDOWN:
-	        	switch ( this->event.key.keysym.sym ) {
-					case SDLK_ESCAPE:
-						play = false;
-						break;
-					default:
-		                break;
-				}
-	        	break;
-	      	case SDL_KEYUP:
-	       		switch ( this->event.key.keysym.sym ) {
-					default:
-		               	break;
-				}
-	        	break;
-	        case SDL_MOUSEMOTION:
-	        	x = this->event.motion.x;
-	        	y = this->event.motion.y;
-	        	for (int i = 0; i < NUM_MENU_ITEMS; i++) {
-	        		if (x >= pos[i].x && x <= pos[i].x + this->menuTextTextures[i].getWidth() &&
-	        			y >= pos[i].y && y <= pos[i].y + this->menuTextTextures[i].getHeight()) {
-	        			if (!selected[i]) {
-	        				selected[i] = true;
-	        			}
-	        		} else {
-	        			if (selected[i]) {
-	        				selected[i] = false;
-	        			}	        			
-	        		}
-	        	}
-	        	break;
-	        case SDL_MOUSEBUTTONDOWN:
-	        	x = this->event.button.x;
-	        	y = this->event.button.y;
-	        	for (int i = 0; i < NUM_MENU_ITEMS; i++) {
-	        		if (x >= pos[i].x && x <= pos[i].x + this->menuTextTextures[i].getWidth() &&
-	        			y >= pos[i].y && y <= pos[i].y + this->menuTextTextures[i].getHeight()) {
-	        			this->update(i);
-	        		}
-	        		play = false;
-	        	}
+	SDL_WaitEvent( &this->event );
+	this->game->window->handleEvent(this->event);
+		switch ( this->event.type ) {
+      	case SDL_KEYDOWN:
+            keyCode = this->event.key.keysym.sym;
+            if (keyCode == SDLK_ESCAPE) {
+                play = false;
+                break;
+            }
 
-	        	break;
-	        case SDL_WINDOWEVENT:
-	        	switch (this->event.window.event) {
-		    		case SDL_WINDOWEVENT_RESIZED:
-		    			this->level->levelTexture.setDimensions(this->game->window->getWidth(), 
-		    													this->game->window->getHeight());
-		    			break;
-		        }
-	        	break;
-			case SDL_QUIT:
-				play = false;
-				break;
-	      	default:
-	        	break;
-    	}
+            for (int i = 0; i < NUM_TEXT_FIELDS; i++) {
+                if (activeTextbox[i]) {
+
+                    //Handling backspace
+                    if (keyCode == SDLK_BACKSPACE && textInput[i].length() > 0) {
+                        textInput[i].pop_back();
+                    }
+
+                    //Handle copy
+                    else if (keyCode == SDLK_c && SDL_GetModState() & KMOD_CTRL) {
+                        SDL_SetClipboardText( textInput[i].c_str() );
+                    }
+
+                    //Handle paste
+                    else if (keyCode == SDLK_v && SDL_GetModState() & KMOD_CTRL) {
+                        textInput[i] = SDL_GetClipboardText();
+                    }
+                }
+            }
+        	break;
+        case SDL_TEXTINPUT:
+            for (int i = 0; i < NUM_TEXT_FIELDS; i++) {
+                if (activeTextbox[i]) {
+                    if (!((this->event.text.text[ 0 ] == 'c' || this->event.text.text[ 0 ] == 'C') &&
+                          (this->event.text.text[ 0 ] == 'v' || this->event.text.text[ 0 ] == 'V') &&
+                           SDL_GetModState() & KMOD_CTRL)) {
+                        if (textInput[i].length() < maxLength) {
+                            textInput[i] += this->event.text.text;
+                        }
+                    }
+                }
+            }
+            break;
+      	case SDL_KEYUP:
+       		switch ( this->event.key.keysym.sym ) {
+				default:
+	               	break;
+			}
+        	break;
+        case SDL_MOUSEMOTION:
+        	x = this->event.motion.x;
+        	y = this->event.motion.y;
+        	for (int i = 0; i < NUM_MENU_ITEMS; i++) {
+        		if (x >= pos[i].x && x <= pos[i].x + this->menuTextTextures[i].getWidth() &&
+        			y >= pos[i].y && y <= pos[i].y + this->menuTextTextures[i].getHeight()) {
+                    selected[i] = true;
+        		} else {
+                    selected[i] = false;
+        		}
+        	}
+        	break;
+        case SDL_MOUSEBUTTONDOWN:
+        	x = this->event.button.x;
+        	y = this->event.button.y;
+        	for (int i = 0; i < NUM_MENU_ITEMS; i++) {
+                if (selected[i]) {
+                    this->update(i);
+        			play = false;
+                }
+        	}
+        	for (int i = 0; i < NUM_TEXT_FIELDS; i++) {
+        		if (x >= textboxPos[i].x && x <= textboxPos[i].x + textboxPos[i].w &&
+        			y >= textboxPos[i].y && y <= textboxPos[i].y + textboxPos[i].h) {
+                    activeTextbox[i] = true;
+        		} else {
+                    activeTextbox[i] = false;
+        		}
+        	}
+
+        	break;
+        case SDL_WINDOWEVENT:
+        	switch (this->event.window.event) {
+	    		case SDL_WINDOWEVENT_RESIZED:
+	    			this->level->levelTexture.setDimensions(this->game->window->getWidth(),
+	    													this->game->window->getHeight());
+	    			break;
+	        }
+        	break;
+		case SDL_QUIT:
+			play = false;
+			break;
+      	default:
+        	break;
 	}
 }
 
 void GameStateMenu::update(const float& delta) {
-	
+
 	// TEMP: need to handle changing states better (use correct numbering in Game.cpp)
 	//Add a switch statement instead
 	if(delta == 0) {
@@ -188,70 +190,144 @@ void GameStateMenu::update(const float& delta) {
 	}
 }
 
+void GameStateMenu::positionElements() {
+
+    int         windowWidth     = this->game->window->getWidth();
+    int         windowHeight    = this->game->window->getHeight();
+    int         maxTextWidth    = 0;
+    int         maxTextHeight   = 0;
+    int         vertPadding     = 50;
+    int         horzPadding     = 10;
+    std::string longestString   = "";
+    char        largestChar     = 'W';
+
+
+    if(!TTF_WasInit() && TTF_Init()==-1) {
+        printf("TTF_Init: %s\n", TTF_GetError());
+        exit(1);
+    }
+    //Calculate the pixel length and height of the largest possible string
+    longestString.resize(maxLength+1, largestChar);
+	TTF_SizeText(textboxFont, longestString.c_str(), &maxTextWidth, &maxTextHeight);
+
+    //Position the menu text
+    pos[JOIN].x     = windowWidth/2  - this->menuTextTextures[JOIN].getWidth()/2;
+    pos[JOIN].y     = windowHeight/2 + this->menuTextTextures[JOIN].getHeight();
+    pos[OPTIONS].x  = windowWidth/2  - this->menuTextTextures[OPTIONS].getWidth()/2;
+    pos[OPTIONS].y  = windowHeight/2 + this->menuTextTextures[OPTIONS].getHeight()*2.2;
+
+    //Create a textbox for the server IP
+    textboxPos[IP].w = maxTextWidth;
+    textboxPos[IP].h = maxTextHeight;
+    textboxPos[IP].x = windowWidth/2  - textboxPos[IP].w/2;
+    textboxPos[IP].y = pos[JOIN].y - vertPadding;
+
+    //Create a textbox for the Username
+    textboxPos[USERNAME].w = maxTextWidth;
+    textboxPos[USERNAME].h = maxTextHeight;
+    textboxPos[USERNAME].x = windowWidth/2  - textboxPos[USERNAME].w/2;
+    textboxPos[USERNAME].y = textboxPos[IP].y - vertPadding;
+
+    //Position the Textbox titles to the left of their respective text box
+    textboxTitlePos[IP].x = textboxPos[IP].x - this->textboxTitleTextures[IP].getWidth() - horzPadding;
+    textboxTitlePos[IP].y = textboxPos[IP].y;
+    textboxTitlePos[USERNAME].x = textboxPos[USERNAME].x - this->textboxTitleTextures[USERNAME].getWidth() - horzPadding;
+    textboxTitlePos[USERNAME].y = textboxPos[USERNAME].y;
+
+//    std::cout << "before textboxTitlePos[" << "IP" << "]X:" << textboxTitlePos[IP].x << "\n";
+//    std::cout << "before textboxTitlePos[" << "IP" << "]Y:" << textboxTitlePos[IP].y << "\n";
+}
+
 void GameStateMenu::render() {
 	//Only draw when not minimized
 	if ( !this->game->window->isMinimized() ) {
 
 		//Clear screen
-		SDL_SetRenderDrawColor( this->game->renderer, 0xFF, 0xFF, 0xFF, 0xFF );
 		SDL_RenderClear( this->game->renderer );
-		int windowWidth = this->game->window->getWidth();
-		int windowHeight = this->game->window->getHeight();
 
-		//Render textures
-		this->level->levelTexture.render(this->game->renderer, 
-										 0-this->camera->getX(),
-										 0-this->camera->getY());
-	
-		SDL_Color textColor = { 0, 0, 0, 255 };
+ 		//Render textures
+		this->level->levelTexture.render(this->game->renderer, 0-this->camera->getX(), 0-this->camera->getY());
 
-
-		//Render FPS text
-		if ( !this->frameFPSTextTexture.loadFromRenderedText( this->frameTimeText.str().c_str(),
-											  textColor, this->game->renderer, this->frameFont ) ) {
-			printf( "Unable to render FPS texture!\n" );
-		}		
-		this->frameFPSTextTexture.render(this->game->renderer, (windowWidth - this->frameFPSTextTexture.getWidth()), 0);
-
+        //Position all screen elements in the window
+		positionElements();
+        //sleep(1);
 
 		//Render Menu text
 		for (int i = 0; i < NUM_MENU_ITEMS; i++) {
 			if(selected[i]) {
+//                renderText(this->menuTextTextures[i], this->menuItems[i], fontColors[GREEN], this->menuFont, pos[i]);
+
 				if ( !this->menuTextTextures[i].loadFromRenderedText( this->menuItems[i],
-											  fontColors[1], this->game->renderer, this->menuFont ) ) {
-					printf( "Unable to render menu text texture!\n" );				
+											  fontColors[GREEN], this->game->renderer, this->menuFont ) ) {
+					printf( "Unable to render menu text texture!\n" );
 				}
 			} else {
 				if ( !this->menuTextTextures[i].loadFromRenderedText( this->menuItems[i],
-											  fontColors[0], this->game->renderer, this->menuFont ) ) {
-					printf( "Unable to render menu text texture!\n" );				
+											  fontColors[WHITE], this->game->renderer, this->menuFont ) ) {
+					printf( "Unable to render menu text texture!\n" );
 				}
+//                renderText(this->menuTextTextures[i], this->menuItems[i], fontColors[WHITE], this->menuFont, pos[i]);
 			}
 			this->menuTextTextures[i].render(this->game->renderer, pos[i].x, pos[i].y);
 		}
 
-		//Position the menu text
-		pos[0].x = windowWidth/2 - this->menuTextTextures[0].getWidth()/2;
-		pos[0].y = windowHeight/2 - this->menuTextTextures[0].getHeight();
-		pos[1].x = windowWidth/2 - this->menuTextTextures[1].getWidth()/2;
-		pos[1].y = windowHeight/2 + this->menuTextTextures[1].getHeight();
+        //Change the color of the textbox when active
+        //Used so User knows when textbox is can accept input
+		for (int i = 0; i < NUM_TEXT_FIELDS; i++) {
+            if (activeTextbox[i]) {
+                SDL_SetRenderDrawColor(this->game->renderer, YELLOW_RGB[0], YELLOW_RGB[1], YELLOW_RGB[2], OPAQUE);
+                SDL_RenderFillRect(this->game->renderer, &textboxPos[i]);
+            } else {
+                SDL_SetRenderDrawColor(this->game->renderer, WHITE_RGB[0], WHITE_RGB[1], WHITE_RGB[2], OPAQUE);
+                SDL_RenderFillRect(this->game->renderer, &textboxPos[i]);
+            }
+		}
 
-		
+		for (int i = 0; i < NUM_TEXT_FIELDS; i++) {
+            if (!textInput[i].empty()) {
+//                renderText(this->textboxTextures[i], this->textInput[i].c_str(), fontColors[BLACK], this->textboxFont, textboxPos[i].x + 5, textboxPos[i].y - 7);
+
+                if ( !this->textboxTextures[i].loadFromRenderedText( this->textInput[i],
+                                              fontColors[BLACK], this->game->renderer, this->textboxFont ) ) {
+                    printf( "Unable to render textbox text texture!\n" );
+                }
+                this->textboxTextures[i].render(this->game->renderer, textboxPos[i].x + 5, textboxPos[i].y - 7);
+			}
+		}
+
+		//Render textbox titles
+//		for (int i = 0; i < NUM_TEXT_FIELDS; i++) {
+////            renderText(this->textboxTitleTextures[i], this->textboxTitles[i], fontColors[WHITE], this->headingFont, textboxTitlePos[i].x, textboxTitlePos[i].y);
+//            if ( !this->textboxTitleTextures[i].loadFromRenderedText( this->textboxTitles[i],
+//                                        fontColors[WHITE], this->game->renderer, this->headingFont ) ) {
+//                printf( "Unable to render textbox texture!\n" );
+//            }
+//
+//			this->textboxTitleTextures[i].render(this->game->renderer, textboxTitlePos[i].x, textboxTitlePos[i].y);
+//		}
+
 		//Update screen
 		SDL_RenderPresent( this->game->renderer );
 	}
 }
 
+void GameStateMenu::renderText(LTexture fontTexture, const char* text, SDL_Color color, TTF_Font* font, int x, int y) {
+    if ( !fontTexture.loadFromRenderedText( text, color, this->game->renderer, font ) ) {
+                printf( "Unable to render text texture!\n" );
+    }
+    fontTexture.render(this->game->renderer, x, y);
+}
+
 GameStateMenu::~GameStateMenu() {
-	
+
 	// Free texture and font
-	this->frameFPSTextTexture.free();
 	this->menuTextTextures[1].free();
 	this->menuTextTextures[0].free();
-	TTF_CloseFont(this->frameFont);
+	TTF_CloseFont(this->textboxFont);
+	TTF_CloseFont(this->headingFont);
 	TTF_CloseFont(this->menuFont);
 	this->menuFont = NULL;
-	this->frameFont = NULL;
+	this->textboxFont = NULL;
+	this->headingFont = NULL;
 
 }
- 
--- src/GameStateMenu.h
+++ src/GameStateMenu.h
@@ -12,21 +12,20 @@
 class GameStateMenu : public GameState {
 public:
 
+    GameStateMenu(Game* g) : GameState(g) {};
+	virtual ~GameStateMenu();
+
 	virtual bool load();
 	virtual void loop();
 
-	GameStateMenu(Game* g) : GameState(g) {};
-	virtual ~GameStateMenu();
+	Level* level    = NULL;
+	Camera* camera  = NULL;
 
-	Level* level = NULL;
-	Camera* camera = NULL;
-	
-	// Frame Display
-	std::stringstream frameTimeText;
-	TTF_Font* frameFont = NULL;
-	TTF_Font* menuFont = NULL;
+	TTF_Font* headingFont   = NULL;
+	TTF_Font* textboxFont   = NULL;
+	TTF_Font* menuFont      = NULL;
 	LTexture frameFPSTextTexture;
-	
+
 private:
 
 	virtual void sync();
@@ -34,12 +33,37 @@ private:
 	virtual void update(const float& delta);
 	virtual void render();
 
-	static const int NUM_MENU_ITEMS = 2;
-	const char* menuItems[NUM_MENU_ITEMS] = {"join", "options"};
-	bool selected[NUM_MENU_ITEMS] = {false,false};
-	SDL_Color fontColors[2] = {{255,255,255}, {54,96,81}};	
-	SDL_Rect pos[NUM_MENU_ITEMS];
-	LTexture menuTextTextures[NUM_MENU_ITEMS];
+	void positionElements();
+	void renderText(LTexture fontTexture, const char* text, SDL_Color color, TTF_Font* font, int x, int y);
+
+	static const int 	NUM_MENU_ITEMS 				= 2;
+	static const int 	JOIN         				= 0;
+	static const int 	OPTIONS        				= 1;
+	const char* 		menuItems[NUM_MENU_ITEMS] 	= {"join", "options"};
+	bool 				selected[NUM_MENU_ITEMS] 	= {false,false};
+	SDL_Rect 			pos[NUM_MENU_ITEMS];
+	LTexture 			menuTextTextures[NUM_MENU_ITEMS];
+
+	static const int 	NUM_TEXT_FIELDS				        = 2;
+	static const int 	IP         				            = 0;
+	static const int 	USERNAME        				    = 1;
+	const char* 		textboxTitles[NUM_TEXT_FIELDS]      = {"Server IP:", "Username:"};
+	bool 				activeTextbox[NUM_MENU_ITEMS] 	    = {false,false};
+	SDL_Rect 			textboxTitlePos[NUM_TEXT_FIELDS];
+	SDL_Rect 			textboxPos[NUM_TEXT_FIELDS];
+	LTexture 			textboxTitleTextures[NUM_TEXT_FIELDS];
+
+	std::string         textInput[NUM_TEXT_FIELDS]  = {"IP Address:", "Username"};
+	const size_t        maxLength                   = 15;
+	LTexture 			textboxTextures[NUM_TEXT_FIELDS];
+
+	SDL_Color 			fontColors[3]   = {{255,255,255}, {54,96,81}, {0,0,0}};
+	const size_t        WHITE           = 0;
+	const size_t        GREEN           = 1;
+	const size_t        BLACK           = 2;
+	const size_t        OPAQUE          = 255;
+	const size_t        WHITE_RGB[3]    = {255, 255, 255};
+	const size_t        YELLOW_RGB[3]   = {148, 139, 77};
 };
 
 #endif
