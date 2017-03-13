7455bac4fe0920d1d38386e3cac9818c21654547
Matt Goerwell <superninja246@hotmail.com>
2017-03-13 10:53:25 -0700

    indentation fix, made ctor default

--- src/Consumable.cpp
+++ src/Consumable.cpp
@@ -5,7 +5,7 @@
 
 //Assignment constructor that prints out when it's used for record tracking.
 Consumable::Consumable(LTexture gameTexture, LTexture interfaceTexture) : 
-inGameTexture(gameTexture), UiTexture(interfaceTexture) {
+        inGameTexture(gameTexture), UiTexture(interfaceTexture) {
     printf("Create Consumable\n");
 }
 //Destructor. Prints out use for testing purposes
--- src/Consumable.h
+++ src/Consumable.h
@@ -13,14 +13,14 @@ public:
     //constructor that will assign the right textures to the consumable
     Consumable(LTexture gameTexture, LTexture interfaceTexture);
     //default constructor to be used for testing. Should be removed in final build.
-    Consumable(){};
+    Consumable() = default;
     //Copy ctor
     Consumable(const Consumable& c);
     //standard destructor
     virtual ~Consumable();
     //abstarct method that must be filled in for each consumable type
     virtual void OnConsume() = 0; 
-    
+
 protected:
     //texture to be displayed when consumable is on the ground as part of a consumableDrop
     LTexture inGameTexture;
