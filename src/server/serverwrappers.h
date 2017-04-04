89551051b25ab502d5ee76e64a2b793f1780f0d8
John Agapeyev <jagapeyev@gmail.com>
2017-04-04 09:08:31 -0700

    Reorganized serverwrappers.h to have a better logical format
    Signed-off-by: John Agapeyev <jagapeyev@gmail.com>

--- src/server/serverwrappers.h
+++ src/server/serverwrappers.h
@@ -6,24 +6,31 @@
 
 #include "server.h"
 
+//Simple wrappers that wrap api calls
 int createSocket(const bool useUDP, const bool nonblocking);
-int32_t getPlayerId();
-void transitionToGameStart();
-void sendTCPClientMessage(const int32_t id, const bool isConnectMessage, const char *mesg, const size_t mesgSize);
-void processTCPMessage(const int sock, const char *buff, const size_t nbytes);
 sockaddr_in bindSocket(const int sock, const unsigned long ip, const unsigned short port);
 int createEpollFD();
 void addEpollSocket(const int epollfd, const int sock, epoll_event *ev);
 void setSockNonBlock(const int sock);
 void fillMulticastAddr(sockaddr_in& addr);
+epoll_event *createEpollEventList();
+
+//Game related calls
+int32_t getPlayerId();
+void transitionToGameStart();
+
+//More network oriented functions
+void sendTCPClientMessage(const int32_t id, const bool isConnectMessage, const char *mesg, const size_t mesgSize);
+void processTCPMessage(const int sock, const char *buff, const size_t nbytes);
 void handleIncomingTCP(const int epollfd);
 void readTCP(const int sock);
 void readUDP(const int sock, sockaddr *servaddr, socklen_t *servAddrLen);
 int waitForEpollEvent(const int epollfd, epoll_event *events);
-epoll_event *createEpollEventList();
+bool rawClientSend(const int sock, const char *outBuff, const size_t bufferSize);
+
+//Networking handshake calls
 void processClientUsername(const int sock, const char *buff, std::pair<const int32_t, PlayerJoin>& client);
 void processCommandMessage(const int32_t idReceived, const char *buff, const int nbytes);
-bool rawClientSend(const int sock, const char *outBuff, const size_t bufferSize);
 bool checkIfAllClientsAreReady();
 void processReadyMessage(const int32_t idReceived);
 void processUnreadyMessage(const int32_t idReceived);
